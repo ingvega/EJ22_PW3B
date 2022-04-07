c176e51102c8039f4ac5788be756b67c7fbd774e
ingvega <isc.p.vega@gmail.com>
2022-04-07 12:08:00 -0500

    Se integra ejemplo previo a la implmentación del cambio de velocidad

--- unidad4/AnimacionJS/scripts.js
+++ unidad4/AnimacionJS/scripts.js
@@ -16,23 +16,52 @@
 
 alumno.nombre*/
 
+//DOM (Document Object Model)
+//Se ejecuta cuando ya está listo el árbol que representa
+//todo el desglose de elementos del documento
+//document.addEventListener("DOMContentLoaded",function(){
+    //Configurar las animaciones
+//});
 
-var letras=document.getElementsByClassName('letra');
+//Se ejecuta cuando ya está listo el DOM y además todos 
+//los recursos (imagenes, videos, estilos, librerías) 
+//locales y externos se han cargado
+window.addEventListener("load",function(){
+    //Configurar las animaciones
+    var letras=document.getElementsByClassName('letra');
 
-/*letras[0].animate(
-    [{keyframes}],
-    {configuración de la animación}
-);*/
-letras[0].animate(
-    [
-        //0% o from
-        { transform:'rotateY(180deg)', backgroundColor:'white' },
-        //100% o to
-        { transform:'rotateY(0deg)' , backgroundColor:'#ddd' }
-    ],
-    {
-        duration:2000,
-        delay:1000,
-        fill: 'forwards'
+    /*letras[0].animate(
+        [{keyframes}],
+        {configuración de la animación}
+    );*/
+    var retardo=1000;
+    var animaciones=[];
+    for(var i=0;i<letras.length;i++){
+        animaciones[i]=letras[i].animate(
+            [
+                //0% o from
+                { transform:'rotateY(180deg)', backgroundColor:'white' },
+                //100% o to
+                { transform:'rotateY(0deg)' , backgroundColor:'#ddd' }
+            ],
+            {
+                duration:2000,
+                delay:retardo,
+                fill: 'forwards'
+            }
+        );
+        retardo+=1000;
     }
-);
+
+    
+});
+
+
+/*
+var boton=document.getElementById("btnAccion");
+boton.addEventListener("change",function(){
+    //Acciones a ejecutar después del click
+});
+*/
+
+
